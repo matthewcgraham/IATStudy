33e95f60b9bb21377ab00305782726c08c542c0c
matthewcgraham <36674439+matthewcgraham@users.noreply.github.com>
2021-02-05 14:48:36 -0800

    Update and rename Gend_E4_2.js to Gend_E4_3.js

--- Gend_E4_3.js
+++ Gend_E4_3.js
@@ -1,4 +1,4 @@
-define(['pipAPI','https://cdn.jsdelivr.net/gh/matthewcgraham/IATStudy/IAT_E4Page_t2.js'], function(APIConstructor, iatExtension){
+define(['pipAPI','https://cdn.jsdelivr.net/gh/matthewcgraham/IATStudy/IAT_E4Page_t4.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 return iatExtension({
