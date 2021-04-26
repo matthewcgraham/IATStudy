75aa7715af51bc161f7f1bea37d49e8a306eafd1
matthewcgraham <36674439+matthewcgraham@users.noreply.github.com>
2021-04-25 21:02:43 -0700

    Update and rename Gend11.js to Gend12.js

--- Gend12.js
+++ Gend12.js
@@ -1,4 +1,4 @@
-define(['pipAPI','https://cdn.jsdelivr.net/gh/matthewcgraham/IATStudy/IAT_T11.js'], function(APIConstructor, iatExtension){
+define(['pipAPI','https://cdn.jsdelivr.net/gh/matthewcgraham/IATStudy/IAT_T12.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 return iatExtension({
