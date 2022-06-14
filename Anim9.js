1cee394926747452c1a02d0df5d7606f42016507
matthewcgraham <36674439+matthewcgraham@users.noreply.github.com>
2022-06-14 09:22:07 -0700

    Update and rename Anim8.js to Anim9.js

--- Anim9.js
+++ Anim9.js
@@ -1,4 +1,4 @@
-define(['pipAPI','https://cdn.jsdelivr.net/gh/matthewcgraham/IATStudy/IAT_T15.js'], function(APIConstructor, iatExtension){
+define(['pipAPI','https://cdn.jsdelivr.net/gh/matthewcgraham/IATStudy/IAT_T16.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 return iatExtension({
